60c3aec52814eb026d6d437f81ddd81b1c0cd60f
aryk <161099372+zoolpher@users.noreply.github.com>
2025-06-09 16:16:39 +0530

    Update interactionSC.js
    Codes used by @kshitijofficial are ethers v5 compatible but now we use ethers v6.
    I've updated codes that are compatible with ethers v6

--- interactionSC.js
+++ interactionSC.js
@@ -1,6 +1,17 @@
+
+// According to ethers v6
 //0x417997417dd95f45bb4986abff5dfae5b5b0a34a
-const { ethers } = require("ethers");
-const provider = new ethers.providers.JsonRpcProvider(`Add you infura key`);
+
+const { ethers, formatEther } = require("ethers");
+/*
+formatEther:
+    a utility function used to convert a value in wei (the smallest unit of Ether) 
+    into a human-readable string in ETH
+    1 ETH = 1,000,000,000,000,000,000 wei (10¹⁸ wei)
+*/
+
+const provider = new ethers.JsonRpcProvider(`Add you infura key`);                // v6
+// const provider = new ethers.providers.JsonRpcProvider(`Add you infura key`);   // v5
 
 const walletAddress = "0x417997417dd95f45bb4986abff5dfae5b5b0a34a";
 const walletAbi = [
@@ -104,20 +115,30 @@ const contractIntreaction = async () => {
     provider
   );
 
-  // const contractName = await walletContract.name();
-  // console.log("Contract Name:", contractName);
+  
+  const contractName = await walletContract.name();
+  console.log("Contract Name:", contractName);
 
-  // const num = await walletContract.getValue();
-  // console.log("Number Value:", String(num));
+  
+  const num = await walletContract.getValue();
+  console.log("Number Value:", num.toString());     // v6   (convert BigInt to string)
+  // console.log("Number Value:", Number(num));     // v6   (convert BigInt to number)
+  // console.log("Number Value:", String(num));     // v5
 
+  
   const contractBalance = await walletContract.contractBalance();
-  const balethContract = ethers.utils.formatEther(contractBalance);
-  console.log("Contract Balance:", balethContract);
+  const balethContract = formatEther(contractBalance);                   // v6  
+  // const balethContract = Number(contractBalance) / 1e18;              // v6   (alternate method) 
+  // const balethContract = ethers.utils.formatEther(contractBalance);   // v5
 
-  // const userBalance = await walletContract.accountBalance(
-  //   "0xBE4024Fa7461933F930DD3CEf5D1a01363E9f284"
-  // );
-  // const balethUser = ethers.utils.formatEther(userBalance);
-  // console.log("User Balance:", balethUser);
+  
+  const userBalance = await walletContract.accountBalance(
+    "0xBE4024Fa7461933F930DD3CEf5D1a01363E9f284"
+  );
+  const balethUser = formatEther(userBalance);                     // v6  
+  // const balethUser = Number(userBalance) / 1e18;                // v6   (alternate method)   
+  // const balethUser = ethers.utils.formatEther(userBalance);     // v5
+  console.log("User Balance:", balethUser);
 };
+
 contractIntreaction();
